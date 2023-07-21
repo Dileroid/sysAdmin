1dd6fb95bbfc3ea6f7ba7d43c86d7f40fa09c92b
Dileroid <derksen299@gmail.com>
2023-07-21 15:52:34 +0300

    Add files via upload

---
+++ mtscRds.py
@@ -0,0 +1,8 @@
+import os
+
+pcName = input('Введите имя ПК:')
+qwinsta = 'cmd /c qwinsta /server:' + (pcName)
+os.system(qwinsta)
+userId = input('Введите ID пользователя:')
+mstsc = 'cmd /k mstsc /shadow:' + (userId) + '/v:' + (pcName) + '/controyl /noConsentPrompt'
+os.system(mstsc)

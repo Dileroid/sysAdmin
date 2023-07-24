661e09b6f44fd5e5a03c1144518617ad46faa78d
Dileroid <derksen299@gmail.com>
2023-07-24 08:12:16 +0300

    Update mtscRds.py

--- mtscRds.py
+++ mtscRds.py
@@ -4,5 +4,5 @@ pcName = input('Введите имя ПК:')
 qwinsta = 'cmd /c qwinsta /server:' + (pcName)
 os.system(qwinsta)
 userId = input('Введите ID пользователя:')
-mstsc = 'cmd /k mstsc /shadow:' + (userId) + '/v:' + (pcName) + '/controyl /noConsentPrompt'
+mstsc = 'cmd /k mstsc /shadow:' + (userId) + '/v:' + (pcName) + '/control /noConsentPrompt'
 os.system(mstsc)

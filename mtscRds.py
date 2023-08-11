d1577a165184be6d49d5b3b78659c732a265a4ee
Dileroid <derksen299@gmail.com>
2023-08-11 14:39:47 +0300

    updated

--- mtscRds.py
+++ mtscRds.py
@@ -1,8 +1,20 @@
 import os
+from time import sleep
+from sys import exit
+from subprocess import check_output
 
 pcName = input('Введите имя ПК:')
 qwinsta = 'cmd /c qwinsta /server:' + (pcName)
 os.system(qwinsta)
 userId = input('Введите ID пользователя:')
-mstsc = 'cmd /k mstsc /shadow:' + (userId) + '/v:' + (pcName) + '/control /noConsentPrompt'
+togglecontrol = input('Перехватить контроль?(y/n)')
+if togglecontrol in ('y' , 'ye' , 'yes'):
+    mstsc = 'cmd /c mstsc /v:' + (pcName) + ' /admin' + ' /shadow:' + (userId) + ' /control' + ' /noConsentPrompt'
+elif togglecontrol in ('n' , 'no'):
+    mstsc = 'cmd /c mstsc /v:' + (pcName) + ' /admin' + ' /shadow:' + (userId) + ' /noConsentPrompt'
+else:
+    print('Ошибка выбора на предыдущем шаге')
+    print("Выход...")
+    sleep(1)
+    exit(0)
 os.system(mstsc)

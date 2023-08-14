4bad5c243aeec2f9a063e591b4d7dd1e536c5571
Dileroid <derksen299@gmail.com>
2023-08-14 11:58:10 +0300

    updated

--- mtscRds.py
+++ mtscRds.py
@@ -2,18 +2,22 @@ import os
 from time import sleep
 from sys import exit
 
+def sessionsParsing(pcName):
+    qwinsta = 'cmd /c qwinsta /server:' + (pcName)
+    os.system(qwinsta)
 pcName = input('Введите имя ПК:')
-qwinsta = 'cmd /c qwinsta /server:' + (pcName)
-os.system(qwinsta)
+sessionsParsing(pcName)
+def rdsConnect (userId,togglecontrol):
+    if togglecontrol in ('y' , 'ye' , 'yes'):
+        mstsc = 'cmd /c mstsc /v:' + (pcName) + ' /admin' + ' /shadow:' + (userId) + ' /control' + ' /noConsentPrompt'
+    elif togglecontrol in ('n' , 'no'):
+        mstsc = 'cmd /c mstsc /v:' + (pcName) + ' /admin' + ' /shadow:' + (userId) + ' /noConsentPrompt'
+    else:
+        print('Ошибка выбора на предыдущем шаге')
+        print("Выход...")
+        sleep(1)
+        exit(0)
+    return(mstsc)
 userId = input('Введите ID пользователя:')
 togglecontrol = input('Перехватить контроль?(y/n)')
-if togglecontrol in ('y' , 'ye' , 'yes'):
-    mstsc = 'cmd /c mstsc /v:' + (pcName) + ' /admin' + ' /shadow:' + (userId) + ' /control' + ' /noConsentPrompt'
-elif togglecontrol in ('n' , 'no'):
-    mstsc = 'cmd /c mstsc /v:' + (pcName) + ' /admin' + ' /shadow:' + (userId) + ' /noConsentPrompt'
-else:
-    print('Ошибка выбора на предыдущем шаге')
-    print("Выход...")
-    sleep(1)
-    exit(0)
-os.system(mstsc)
+os.system(rdsConnect(userId,togglecontrol))
